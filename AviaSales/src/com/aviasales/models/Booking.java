5a511c4e912f45cb2603c6058f3469e3900243be
sweb <sweb@innopolis>
2016-07-20 01:15:42 +0300

    added class Booking

---
+++ AviaSales/src/com/aviasales/models/Booking.java
@@ -0,0 +1,12 @@
+package com.aviasales.models;
+
+import java.util.ArrayList;
+
+public class Booking {
+
+	
+	public ArrayList<Ticket> buyTickets(ArrayList<Person> person,Route route){
+		
+		return null;
+	}
+}
