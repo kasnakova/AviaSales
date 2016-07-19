3136bbaaee94546d6bc3dc89faf7d23e601ca792
sweb <sweb@innopolis>
2016-07-19 23:05:29 +0300

    update AnalizatorTests

--- AviaSales/src/com/aviasales/logic/Analizator.java
+++ AviaSales/src/com/aviasales/logic/Analizator.java
@@ -1,15 +1,15 @@
 package com.aviasales.logic;
 
-import com.aviasales.models.Flight;
-import com.aviasales.models.Route;
-import com.aviasales.models.SearchParams;
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.ArrayList;
 
-import org.junit.Test;
+import com.aviasales.data.StorageAdapter;
+import com.aviasales.models.Flight;
+import com.aviasales.models.Route;
+import com.aviasales.models.SearchParams;
 
 /**
  * Contains the algorithm for finding suitable routes depending on search
@@ -32,56 +32,15 @@ public class Analizator {
 	 * @return
 	 * @throws IOException
 	 */
-	@Test
-	public void TestData() throws IOException {
-		ArrayList<Flight> flights = getTestFlightData();
-		assert (9 == flights.size());
-
-	}
-
-	@Test
-	public void TestNumberFindedRoutes() throws IOException {
-
-		SearchParams sp = getTestSearchParams();
-		ArrayList<Route> Routes = searchFlights(sp);
-		assert (Routes.size() == 2);
-	}
+	
 
 	private static ArrayList<Route> result = new ArrayList<Route>();
 
-	private static SearchParams getTestSearchParams() {
-		SearchParams sp = new SearchParams("KAZAN", "KISLOVODSK", LocalDate.of(2016, Month.AUGUST, 14), 2);
 
-		return sp;
-	}
 
-	private static ArrayList<Flight> getTestFlightData() throws IOException {
-
-		ArrayList<Flight> flights = new ArrayList<Flight>();
-		flights.add(new Flight("2353", "KAZAN", "KISLOVODSK", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
-		flights.add(new Flight("2345", "KAZAN", "MOSCOW", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
-		flights.add(new Flight("2352", "MOSCOW", "KISLOVODSK", LocalDateTime.of(2016, Month.AUGUST, 14, 15, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 16, 0), 1000.0, 20));
-		flights.add(new Flight("2346", "KAZAN", "NABEREZHNIE CHELNY", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
-		flights.add(new Flight("2347", "KAZAN", "KRASNODAR", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
-		flights.add(new Flight("2348", "KRASNODAR", "MOSCOW", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
-		flights.add(new Flight("2349", "NABEREZHNIE CHELNY", "MOSCOW", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
-		flights.add(new Flight("2350", "KISLOVODSK", "ROSTOV", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
-		flights.add(new Flight("2351", "KISLOVODSK", "KAZAN", LocalDateTime.of(2016, Month.AUGUST, 15, 12, 0),
-				LocalDateTime.of(2016, Month.AUGUST, 15, 13, 0), 1000.0, 20));
-		// ArrayList<Flight> flights2 = StorageAdapter.loadFlights();
-		return flights;
-	}
-
+	
 	public static ArrayList<Route> searchFlights(SearchParams sp) throws IOException {
-		ArrayList<Flight> flights = getTestFlightData();
+		ArrayList<Flight> flights = StorageAdapter.loadFlights();
 		
 		String	dep=sp.getDep();
 		String	arr=sp.getArr();
@@ -104,8 +63,8 @@ public class Analizator {
 	}
 
 	private static void SearchConnectedFlights(Flight flight,String arr,int personCount) throws IOException {
-		ArrayList<Flight> flights = getTestFlightData();
-
+		ArrayList<Flight> flights = StorageAdapter.loadFlights();
+		
 		for (Flight f : flights) {
 			if ((isConnectingFlight(flight,f)) && (f.getNumberOfFreePlaces() >= personCount)) {
 				if (isEndFlight(f,arr)) {
--- AviaSalesTests/src/com/aviasales/tests/AnalizatorTest.java
+++ AviaSalesTests/src/com/aviasales/tests/AnalizatorTest.java
@@ -1,6 +1,64 @@
 package com.aviasales.tests;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.aviasales.logic.Analizator;
+import com.aviasales.models.Flight;
+import com.aviasales.models.Route;
+import com.aviasales.models.SearchParams;
 
 public class AnalizatorTest {
 	
+	
+	@Test
+	public void TestData() throws IOException {
+		ArrayList<Flight> flights = getTestFlightData();
+		assert (9 == flights.size());
+
+	}
+
+	@Test
+	public void TestNumberFindedRoutes() throws IOException {
+
+		SearchParams sp = getTestSearchParams();
+		ArrayList<Route> Routes = Analizator.searchFlights(sp);
+		assert (Routes.size() == 2);
+	}
+	
+	private static SearchParams getTestSearchParams() {
+		SearchParams sp = new SearchParams("KAZAN", "KISLOVODSK", LocalDate.of(2016, Month.AUGUST, 14), 2);
+
+		return sp;
+	}
+	
+	private static ArrayList<Flight> getTestFlightData() throws IOException {
+
+		ArrayList<Flight> flights = new ArrayList<Flight>();
+		flights.add(new Flight("2353", "KAZAN", "KISLOVODSK", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
+		flights.add(new Flight("2345", "KAZAN", "MOSCOW", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
+		flights.add(new Flight("2352", "MOSCOW", "KISLOVODSK", LocalDateTime.of(2016, Month.AUGUST, 14, 15, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 16, 0), 1000.0, 20));
+		flights.add(new Flight("2346", "KAZAN", "NABEREZHNIE CHELNY", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
+		flights.add(new Flight("2347", "KAZAN", "KRASNODAR", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
+		flights.add(new Flight("2348", "KRASNODAR", "MOSCOW", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
+		flights.add(new Flight("2349", "NABEREZHNIE CHELNY", "MOSCOW", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
+		flights.add(new Flight("2350", "KISLOVODSK", "ROSTOV", LocalDateTime.of(2016, Month.AUGUST, 14, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 14, 13, 0), 1000.0, 20));
+		flights.add(new Flight("2351", "KISLOVODSK", "KAZAN", LocalDateTime.of(2016, Month.AUGUST, 15, 12, 0),
+				LocalDateTime.of(2016, Month.AUGUST, 15, 13, 0), 1000.0, 20));
+		return flights;
+	}
+
 }
